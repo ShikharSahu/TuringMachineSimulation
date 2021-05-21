8efb2a5fca3d1af7388601cffc544b2fd17d41c7
shikk <shikharsahu76@gmail.com>
2021-05-21 18:54:54 +0530

    improves main and adds 2 more input files

--- src/com/shik/Main.java
+++ src/com/shik/Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) {
         try {
             //location where input file is saved
-            TuringMachine turingMachine= new TuringMachine("D:\\javastuff\\flat_assignment\\file1.txt");
+            //add a location relative to your own device
+            TuringMachine turingMachine= new TuringMachine("D:\\javastuff\\file3.txt");
             //makes the turing machine run until it halts
             while (turingMachine.turn());
         }
         catch (Exception e){
             e.printStackTrace();
         }
-
     }
 }
